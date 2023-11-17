8ba153da0b118a130bc276caddcc27a6b56d68d3
akffhaos95 <akfffhaos95@naver.com>
2023-11-17 12:29:41 +0900

    image name condition

--- src/components/CardModules/CardFront.js
+++ src/components/CardModules/CardFront.js
@@ -12,6 +12,18 @@ const CardFront = ({ player }) => {
   const position = player['포지션']
   const award = player['어워즈']
 
+  function setImagebyName() {
+    if (name === "김형준") {
+      if (position === "P") {
+        return name+"_투수"
+      } else {
+        return name+"_타자"
+      }
+    } else {
+      return name
+    }
+  }
+  
   var middleName = en_name.split(" ");
 
   return (
@@ -20,8 +32,8 @@ const CardFront = ({ player }) => {
           <BackText1>{middleName[1]}{middleName[2]}</BackText1> 
           <BackText2>RASCAL</BackText2>
           <FrontTopBox />
-          <NameBox name={name} number={number} position={position} award={award}/>
-          <Image src={`${process.env.PUBLIC_URL}/image/${name}.png`}/>
+          <NameBox name={name}  number={number} position={position} award={award}/>
+          <Image src={`${process.env.PUBLIC_URL}/image/${setImagebyName()}.png`}/>
           <FrontBottomBox />
           <Sign sign={ player['명대사']} signStyle={ player['명대사 스타일']} />
         </CardTemplate>
