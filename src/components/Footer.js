8c3e69efd9c8abade57a206b870d5560a64471f1
akffhaos95 <akfffhaos95@naver.com>
2024-11-15 20:12:44 +0900

    2024 Version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { styled } from '@mui/system';
-import { Typography, Box } from '@mui/material';
+import { Box, Typography } from "@mui/material";
+
+import React from "react";
+import { styled } from "@mui/system";
 
 const FooterBox = styled(Box)({
-  backgroundColor: '#283759', // 배경색 설정
-  color: 'white',
-  padding: '10px 0',
-  position: 'fixed',
+  backgroundColor: "#283759", // 배경색 설정
+  color: "white",
+  padding: "10px 0",
+  position: "fixed",
   bottom: 0,
-  width: '100%',
-  zIndex: 50
+  width: "100%",
+  zIndex: 50,
 });
 const Footer = () => {
   return (
     <FooterBox>
       <Typography variant="body2" align="center">
-        © 2023 Rascal
+        © 2024 Rascal
       </Typography>
     </FooterBox>
   );
